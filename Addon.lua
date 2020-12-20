3da5715ae3312b96a3e2f95bea5cae1431cf2eb1
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2020-12-20 20:17:29 +0000

    major updates

--- Addon.lua
+++ Addon.lua
@@ -4,29 +4,58 @@
 --   enemy, friendly, all, none.
 -- Author: waldenp0nd
 -- License: Public Domain
--- https://github.com/waldenp0nd/ravNameplates
--- https://www.wowinterface.com/downloads/info24006-RavenousNameplates.html
 ---
-local _, ravNameplates = ...
+local name, ravNameplates = ...
 ravNameplates.name = "Ravenous Nameplates"
-ravNameplates.version = "2.0.0"
+ravNameplates.version = GetAddOnMetadata(name, "Version")
+ravNameplates.github = "https://github.com/waldenp0nd/ravNameplates"
+ravNameplates.curseforge = "https://www.curseforge.com/wow/addons/ravnameplates"
+ravNameplates.wowinterface = "https://www.wowinterface.com/downloads/info24006-RavenousNameplates.html"
+ravNameplates.discord = "https://discord.gg/dNfqnRf2fq"
 
 local defaults = {
-    LOCALE = "enUS"
+    COMMAND = "ravn",
+    LOCALE =  "enUS"
 }
+
+local function prettyPrint(message, full)
+    if full == false then
+        message = message .. ":"
+    end
+    local prefix = "|cff9eb8c9" .. ravNameplates.name .. (full and " " or ":|r ")
+    DEFAULT_CHAT_FRAME:AddMessage(prefix .. message)
+end
+
+local function sendVersionData()
+    C_ChatInfo.SendAddonMessage(name, RAVN_version, "YELL")
+    C_ChatInfo.SendAddonMessage(name, RAVN_version, "PARTY")
+    C_ChatInfo.SendAddonMessage(name, RAVN_version, "RAID")
+    if guild then
+        C_ChatInfo.SendAddonMessage(name, RAVN_version, "GUILD")
+    end
+end
+
+function ensureMacro()
+    local body = "/" .. defaults.COMMAND
+    local numberOfMacros, _ = GetNumMacros()
+    if GetMacroIndexByName(ravNameplates.name) > 0 then
+        EditMacro(GetMacroIndexByName(ravNameplates.name), ravNameplates.name, 134067, body)
+    elseif numberOfMacros < 120 then
+        CreateMacro(ravNameplates.name, 134067, body)
+    else
+        prettyPrint(ravNameplates.locales[ravNameplates.locale].nospace)
+    end
+end
+
 
 local colorAlliance = "0078ff"
 local colorHorde = "b30000"
 local colorNeutral = "f8b700"
 local colorNone = "999999"
 local faction, _ = UnitFactionGroup("player")
+local guild, _, _, _ = GetGuildInfo("player")
 local colorFriendly = faction == "Alliance" and colorAlliance or faction == "Horde" and colorHorde or colorNeutral
 local colorEnemy = faction == "Alliance" and colorHorde or faction == "Horde" and colorAlliance or colorNeutral
-
-local function prettyPrint(message)
-    local prefix = "\124cff9eb8c9" .. ravNameplates.name .. ": \124r"
-    DEFAULT_CHAT_FRAME:AddMessage(prefix .. message)
-end
 
 local function nameplateHandler()
     local showingFriends = GetCVar("nameplateShowFriends") == "1" and true or false
@@ -56,17 +85,21 @@ local function nameplateHandler()
 end
 
 
-SLASH_RAVNAMEPLATES1 = "/ravenousnameplates"
-SLASH_RAVNAMEPLATES2 = "/ravnameplates"
-SLASH_RAVNAMEPLATES3 = "/ravplates"
-SLASH_RAVNAMEPLATES4 = "/ravn"
+SLASH_RAVNAMEPLATES1 = "/" .. defaults.COMMAND
+SLASH_RAVNAMEPLATES2 = "/ravplates"
+SLASH_RAVNAMEPLATES3 = "/ravnameplates"
+SLASH_RAVNAMEPLATES4 = "/ravenousplates"
+SLASH_RAVNAMEPLATES5 = "/ravenousnameplates"
 local function slashHandler(message, editbox)
-    if message == "help" or message == "h" then
-        prettyPrint("")
-        print(ravNameplates.locales[ravNameplates.locale].help[1])
-        print(string.format(ravNameplates.locales[ravNameplates.locale].help[2], ravNameplates.name))
-    elseif message == "version" or message == "v" then
+    local command, argument = strsplit(" ", message)
+    if command == "version" or command == "v" then
         prettyPrint(string.format(ravNameplates.locales[ravNameplates.locale].version, ravNameplates.version))
+        sendVersionData()
+    elseif command == "help" or command == "h" then
+        prettyPrint(ravNameplates.locales[ravNameplates.locale].help[1])
+        print(string.format(ravNameplates.locales[ravNameplates.locale].help[2], ravNameplates.name))
+        print(string.format(ravNameplates.locales[ravNameplates.locale].help[3], ravNameplates.name))
+        print(string.format(ravNameplates.locales[ravNameplates.locale].help[4], ravNameplates.discord))
     elseif UnitAffectingCombat("player") then
         RaidNotice_AddMessage(RaidWarningFrame, ravNameplates.locales[ravNameplates.locale].warning, ChatTypeInfo["RAID_WARNING"])
     else
@@ -75,11 +108,8 @@ local function slashHandler(message, editbox)
 end
 SlashCmdList["RAVNAMEPLATES"] = slashHandler
 
--- Check Installation and Updates on AddOn Load
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("ADDON_LOADED")
-frame:SetScript("OnEvent", function(self, event, arg)
-    if arg == "ravNameplates" then
+local function OnEvent(self, event, arg, ...)
+    if arg == name then
         if event == "ADDON_LOADED" then
             ravNameplates.locale = GetLocale()
             if not ravNameplates.locales[ravNameplates.locale] then
@@ -88,13 +118,31 @@ frame:SetScript("OnEvent", function(self, event, arg)
             if not RAVN_version then
                 prettyPrint(string.format(ravNameplates.locales[ravNameplates.locale].load.install, ravNameplates.name))
             elseif RAVN_version ~= ravNameplates.version then
-                prettyPrint(string.format(ravNameplates.locales[ravNameplates.locale].load.update, ravNameplates.name))
+                prettyPrint(string.format(ravNameplates.locales[ravNameplates.locale].load.update, ravNameplates.version))
             end
             if not RAVN_version or RAVN_version ~= ravNameplates.version then
-                print(ravNameplates.locales[ravNameplates.locale].help[1])
                 print(string.format(ravNameplates.locales[ravNameplates.locale].help[2], ravNameplates.name))
+                print(string.format(ravNameplates.locales[ravNameplates.locale].load.both, defaults.COMMAND))
+                RAV_seenUpdate = false
             end
             RAVN_version = ravNameplates.version
+            C_ChatInfo.RegisterAddonMessagePrefix(name)
+            sendVersionData()
+        elseif event == "CHAT_MSG_ADDON" and RAVN_seenUpdate == false then
+            local message, _ = ...
+            local a, b, c = strsplit(".", ravNameplates.version)
+            local d, e, f = strsplit(".", message)
+            if (d > a) or (d == a and e > b) or (d == a and e == b and f > c) then
+                prettyPrint(string.format(ravNameplates.locales[ravNameplates.locale].load.outofdate, ravNameplates.name))
+                RAVN_seenUpdate = true
+            end
         end
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        ensureMacro()
     end
-end)
+end
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("CHAT_MSG_ADDON")
+f:SetScript("OnEvent", OnEvent)
--- locales.lua
+++ locales.lua
@@ -3,21 +3,26 @@ local _, ravNameplates = ...
 ravNameplates.locales = {
     ["enUS"] = {
         ["help"] = {
-            "Type \124cff9eb8c9/ravn\124r (or, better yet, add it to a macro with keybinds) to cycle around the four settings: enemy, friendly, all, none.",
-            "Check out %s on GitHub or WoWInterface for more info and support!"
+            "Information and How to Use",
+            "This AddOn creates and maintains a macro called |cff9eb8c9%s|r for you under General Macros.", -- ravNameplates.name
+            "Check out |cff9eb8c9%s|r on GitHub, WoWInterface, or Curse for more info and support!", -- ravNameplates.name
+            "You can also get help directly from the author on Discord: %s" -- ravNameplates.discord
         },
         ["load"] = {
-            ["install"] = "Thanks for installing %s!",
-            ["update"] = "Thanks for updating %s!"
+            ["outofdate"] = "There is an update available for |cff9eb8c9%s|r! Please go to GitHub, WoWInterface, or Curse to download.", -- ravNameplates.name
+            ["install"] = "Thanks for installing |cff9eb8c9%s|r!", -- ravNameplates.name
+            ["update"] = "Thanks for updating to |cff9eb8c9v%s|r!", -- ravNameplates.version
+            ["both"] = "Type |cff9eb8c9/%s help|r to familiarize yourself with the addon." -- defaults.COMMAND
         },
         ["toggles"] = {
             ["all"] = "all",
             ["friendly"] = "friendly",
             ["enemy"] = "enemy"
         },
-        ["showing"] = "Showing \124cff%s%s\124r nameplates.",
-        ["hiding"] = "Hiding \124cff%s%s\124r nameplates.",
+        ["showing"] = "Showing |cff%s%s|r nameplates.",
+        ["hiding"] = "Hiding |cff%s%s|r nameplates.",
         ["warning"] = "You cannot change nameplates while in combat!",
         ["version"] = "%s is the current version.",
+        ["nospace"] = "Unfortunately, you don't have enough global macro space for the macro to be created!"
     },
 }
