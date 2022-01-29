ec7d3d4308ab9dfce978530aedf0702e4c18b71c
Walden <20667094+waldenp0nd@users.noreply.github.com>
2022-01-29 14:11:43 +0000

    Update locales.lua

--- locales.lua
+++ locales.lua
@@ -12,7 +12,6 @@ end })
 L.SupportHeading = "Information and How to Use"
 L.Support1 = "This Addon creates and maintains a macro called |cff%1$s%2$s|r for you under General Macros." -- ns.color, ns.name
 L.Support2 = "Check out the Addon on GitHub, WoWInterface, or Curse for more info and support!"
-L.Support3 = "You can also get help directly from the author on Discord: |cff%1$s%2$s|r" -- ns.color, ns.discord
 L.Version = "%s is the current version." -- ns.version
 L.Install = "Thanks for installing |cff%1$sv%2$s|r!" -- ns.color, ns.version
 L.Update = "Thanks for updating to |cff%1$sv%2$s|r!" -- ns.color, ns.version
